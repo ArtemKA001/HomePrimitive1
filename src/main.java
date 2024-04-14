08bd1942a042a0e885417d878b71a5d62f99afd3
Neto test <krayzerexe@yandex.ru>
2024-04-14 13:29:53 +0300

    main

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
-
+        int amount = 5000;
+        int moneyMiles = 20;
+        int miles = amount / moneyMiles;
+        System.out.println(miles);
     }
 }
